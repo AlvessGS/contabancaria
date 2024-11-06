b3ebf9d3a41f8dd5ce032a9f011b8627506244f0
AlvessGS <gabriels.alvess25@gmail.com>
2024-11-05 21:30:46 -0300

    primeiro commit

---
+++ menu.java
@@ -0,0 +1,102 @@
+package conta;
+
+import java.util.Scanner;
+import conta.util.Cores;
+
+public class menu {
+	
+	public static void main(String[] args) {
+		
+		Scanner ler = new Scanner(System.in);
+		int opcao;
+		
+		while(true) {
+			
+			System.out.println(Cores.TEXT_WHITE + Cores.ANSI_BLACK_BACKGROUND
+					           + "************************************************************************************");
+			System.out.println("                                                                                    ");
+			System.out.println("                                 BANCO DO BRAZIL COM Z                              ");
+			System.out.println("                                                                                    ");
+			System.out.println("************************************************************************************");
+			System.out.println("                                                                                    ");
+			System.out.println("                               1 - Criar conta                                      ");
+			System.out.println("                               2 - Listar todas as contas                           ");
+			System.out.println("                               3 - Buscar conta por número                          ");
+			System.out.println("                               4 - Atualizar dados da conta                         ");
+			System.out.println("                               5 - Apagar conta                                     ");
+			System.out.println("                               6 - Sacar                                            ");
+			System.out.println("                               7 - Depositar                                        ");
+			System.out.println("                               8 - Transferir valores entre contas                  ");
+			System.out.println("                               9 - Sair                                             ");
+			System.out.println("                                                                                    ");
+			System.out.println("************************************************************************************");
+			System.out.println("Entre com a opção:                                                                  ");
+			System.out.println("                                                                                    "+Cores.TEXT_RESET);
+														
+			opcao = ler.nextInt();
+			ler.nextLine();
+			
+			switch(opcao) {
+				case 1:
+					System.out.println(Cores.TEXT_WHITE_BOLD + "Opção para criar a conta");
+					break;
+					
+				case 2: 
+					System.out.println(Cores.TEXT_WHITE_BOLD + "Listar todas as contas");
+					break;
+					
+				case 3: 
+					System.out.println(Cores.TEXT_WHITE_BOLD + "Buscar conta pior número");
+					break;
+					
+				case 4:
+					System.out.println(Cores.TEXT_WHITE_BOLD + "Atualizar os dados da conta");
+					break;
+					
+				case 5:
+					System.out.println(Cores.TEXT_WHITE_BOLD + "\nApagar conta");
+					break;
+					
+				case 6:
+					System.out.println(Cores.TEXT_WHITE_BOLD + "\nSacar");
+					break;
+					
+				case 7:
+					System.out.println(Cores.TEXT_WHITE_BOLD + "\nDepositar");
+					break;
+					
+				case 8:
+					System.out.println(Cores.TEXT_WHITE_BOLD + "\nTransferir valores entre Contas");
+					break;
+					
+				case 9:
+					System.out.println(Cores.TEXT_WHITE_BOLD + Cores.ANSI_BLACK_BACKGROUND); 
+					System.out.println("                                                                                    ");
+					System.out.println("Banco do Brazil com Z - O seu futuro começa aqui!                                   ");
+					System.out.println("                                                                                    ");
+					System.out.println("************************************************************************************");
+					System.out.println("                                                                                    ");
+					System.out.println(" Projeto Desenvolvido por: Gabriel Alves                                            ");
+					System.out.println(" Generation Brasil - generation@generation.org                                      ");
+					System.out.println(" github/conteudo                                                                    ");
+					System.out.println("                                                                                    ");
+					System.out.println("************************************************************************************");
+					System.out.println("                                                                                    "+Cores.TEXT_RESET);
+					ler.close();
+					return;
+					
+					
+				default:
+					System.out.println(Cores.TEXT_RED_BOLD + "Opção inválida!");
+			}
+			System.out.println("************************************************************************************");
+			System.out.println("                                                                                    ");
+			System.out.println("Projeto Desenvolvido por: Gabriel Alves                                                                                    ");
+			System.out.println("Generation Brasil - generation@generation.org ");
+			System.out.println("github/conteudo                                                                                  ");
+			System.out.println("                                                                                    ");
+			System.out.println("************************************************************************************");
+		}
+		
+	}
+}
